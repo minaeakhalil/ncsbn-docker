3eda4d8b85822ee02d7278eb3b64b7143f315c65
Mina Khalil <mina.ea.khalil@gmail.com>
2017-03-15 09:26:32 -0500

    Adding some comments

--- server/run.sh
+++ server/run.sh
@@ -2,9 +2,11 @@
 
 cd /var/www/html
 
+# Install TAO dependencies using Composer
 composer install
 
-php /var/www/tao/tao/scripts/taoInstall.php \
+# Install TAO
+php /var/www/html/scripts/taoInstall.php \
         --db_driver pdo_mysql \
         --db_host mysql \
         --db_name taoUnitTest \
@@ -16,22 +18,8 @@ php /var/www/tao/tao/scripts/taoInstall.php \
         --user_pass admin \
         -e taoCe
 
-#chmod -R 0777 /var/www/html/taoQtiItem/views/js/portableSharedLibraries \
-#    && chmod -R 0777 /var/www/html/data \
-#    && chmod -R 0777 /var/www/html/config \
-#    && chmod -R 0777 /var/www/html/tao/views/locales \
-#    && chmod -R 0777 /var/www/html/tao/views/js \
-#    && chmod -R 0777 /var/www/html/tao/views \
-#    && chmod -R 0777 /var/www/html/funcAcl/views/build \
-#    && chmod -R 0777 /var/www/html/taoBackOffice/views/build \
-#    && chmod -R 0777 /var/www/html/taoTestTaker/views/build \
-#    && chmod -R 0777 /var/www/html/taoGroups/views/build \
-#    && chmod -R 0777 /var/www/html/taoResultServer/views/build \
-#    && chmod -R 0777 /var/www/html/taoItems/views/js/test \
-#    && chmod -R 0777 /var/www/html/taoItems/views/build \
-#    && chmod -R 0777 /var/www/html/taoQtiItem/views/js/test \
-#    && chmod -R 0777 /var/www/html/taoQtiItem/views/build \
-#    && chmod -R 0777 /var/www/html/qtiItemPci/views/build \
-#    && chmod -R 0777
+# Run TAO Update right after installation
+php /var/www/html/scripts/taoUpdate
 
+# Run SupervisorD which would start Apache Server
 /usr/bin/supervisord -n -c /etc/supervisord.conf

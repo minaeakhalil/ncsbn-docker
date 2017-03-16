97052a13c800cd22d46ed3221139f51aa5be0219
Mina Khalil <mina.ea.khalil@gmail.com>
2017-03-16 11:01:20 -0500

    Multiple fixes for TAO install

--- server/run.sh
+++ server/run.sh
@@ -3,23 +3,20 @@
 cd /var/www/html
 
 # Install TAO dependencies using Composer
-composer install
 
 # Install TAO
-php /var/www/html/scripts/taoInstall.php \
+composer install \
+    && php /var/www/html/tao/scripts/taoInstall.php \
         --db_driver pdo_mysql \
         --db_host mysql \
         --db_name taoUnitTest \
         --db_user root \
         --db_pass test123 \
         --module_namespace http://sample/first.rdf \
-        --module_url http://tao \
+        --module_url http://tao:8080 \
         --user_login admin \
         --user_pass admin \
-        -e taoCe
-
-# Run TAO Update right after installation
-php /var/www/html/scripts/taoUpdate
-
-# Run SupervisorD which would start Apache Server
-/usr/bin/supervisord -n -c /etc/supervisord.conf
+        -e taoCe \
+    && php /var/www/html/tao/scripts/taoUpdate.php \
+    && yum
+    && /usr/bin/supervisord -n -c /etc/supervisord.conf

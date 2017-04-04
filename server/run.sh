ed9ae93350a19b0cb404de9d120e82a3a2594b99
Mina Khalil <mina.ea.khalil@gmail.com>
2017-04-04 14:52:02 -0500

    Adjusting the TAO Install scripts

--- server/run.sh
+++ server/run.sh
@@ -1,17 +1,25 @@
 #!/usr/bin/env bash
 
-# Install TAO
-composer install -d ${DOCROOT} \
-    && php ${DOCROOT}/tao/scripts/taoInstall.php \
-        --db_driver pdo_mysql \
-        --db_host ${MYSQL_HOST} \
-        --db_name ${TAO_MYSQL_DB} \
-        --db_user ${TAO_MYSQL_USER} \
-        --db_pass ${TAO_MYSQL_PASSWORD} \
-        --module_namespace http://sample/first.rdf \
-        --module_url ${TAO_FULL_URL} \
-        --user_login ${TAO_USER} \
-        --user_pass ${TAO_PASSWORD} \
-        -e taoCe \
-    && php ${DOCROOT}/tao/scripts/taoUpdate.php \
-    && /usr/bin/supervisord -n -c /etc/supervisord.conf
+# This was tested on TAO 3.1.0 sprint15 and TAO 3.0 RC7
+if [ ${TAO_INSTALL} == "yes" ]; then
+    # Git is required for Composer to run
+    yum install -y git
+
+    # Install TAO
+    composer install -d ${DOCROOT} \
+        && php ${DOCROOT}/tao/scripts/taoInstall.php \
+            --db_driver pdo_mysql \
+            --db_host ${MYSQL_HOST} \
+            --db_name ${TAO_MYSQL_DB} \
+            --db_user ${TAO_MYSQL_USER} \
+            --db_pass ${TAO_MYSQL_PASSWORD} \
+            --module_namespace ${TAO_MODULE_URL} \
+            --module_url ${TAO_FULL_URL} \
+            --user_login ${TAO_USER} \
+            --user_pass ${TAO_PASSWORD} \
+            -e taoCe \
+        && php ${DOCROOT}/tao/scripts/taoUpdate.php
+fi
+
+# Startup Supervisord and start whatever processes attached to it
+/usr/bin/supervisord -n -c /etc/supervisord.conf
